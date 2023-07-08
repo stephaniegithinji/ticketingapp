9358e8a5c192d6cdccc0579e5bdfcfb4c79d74cb
B4Ha-LzM17 <andruu.karanja>
2023-07-08 17:58:36 +0000

    Refactor modals.js to improve code readability and maintainability

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -6,17 +6,13 @@ var signupClose = document.getElementById("signupClose");
 //Function to open modal
 signupBtn.onclick = function() {
    signupModal.style.display = "block";
-}
+};
+
 //Function to close modal
 signupClose.onclick = function() {
    signupModal.style.display = "none";
-}
-//Function to close modal when clicked outside
-window.onclick = function(event) {
-   if (event.target === signupModal) {
-      signupModal.style.display = "none";
-   }
-}
+};
+
 //Login
 var signinModal = document.getElementById("signin");
 var signinBtn = document.getElementById("signinBtn");
@@ -25,17 +21,12 @@ var signinClose = document.getElementById("signinClose");
 //Function to open modal
 signinBtn.onclick = function() {
    signinModal.style.display = "block";
-}
+};
+
 //Function to close modal
 signinClose.onclick = function() {
    signinModal.style.display = "none";
-}
-//Function to close modal when clicked outside
-window.onclick = function(event) {
-   if (event.target === signinModal) {
-      signinModal.style.display = "none";
-   }
-}
+};
 
 //Contact Us
 var contactModal = document.getElementById("contactus");
@@ -45,17 +36,25 @@ var contactClose = document.getElementById("contactClose");
 //Function to open modal
 contactUsBtn.onclick = function() {
    contactModal.style.display = "block";
-}
+};
+
 //Function to close modal
 contactClose.onclick = function() {
    contactModal.style.display = "none";
-}
+};
+
 //Function to close modal when clicked outside
 window.onclick = function(event) {
+   if (event.target === signupModal) {
+      signupModal.style.display = "none";
+   }
+   if (event.target === signinModal) {
+      signinModal.style.display = "none";
+   }
    if (event.target === contactModal) {
       contactModal.style.display = "none";
    }
-}
+};
 
 
 
