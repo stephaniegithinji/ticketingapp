0ed531b2f9b6e13c5c5ef0406fee444dbd7a14a6
B4Ha-LzM17 <andruu.karanja>
2023-07-12 20:28:37 +0000

    Refactor the client_functions.php file by adding new methods for user registration and login

--- assets/php/client_functions.php
+++ assets/php/client_functions.php
@@ -5,11 +5,48 @@ require_once  '../../connect/config.php';
 
 class Client extends Db
 {
-    public function user_exists($email){
-        $sql = "SELECT email FROM users WHERE email = :email";
+	/**
+	 * Check if a user with the specified email  exists in the users table
+	 *
+	 * @param string $email The service number of the user to check
+	 * @return int The number of row(s) in the users table that match the given email
+	 */
+	public function user_exists($email)
+	{
+		$sql = "SELECT email FROM users WHERE email = :email";
 		$stmt = $this->conn->prepare($sql);
 		$stmt->execute(['email' => $email]);
 		$result = $stmt->fetch(PDO::FETCH_ASSOC);
 		return $result;
-    }
+	}
+	/**
+	 * Registers a new user in the database.
+	 * 
+	 * @param string $name The name of the user
+	 * @param string $email The email of the user
+	 * @param string $password The password of the user
+	 * @return bool Returns true on successful registration, false otherwise
+	 */
+	public function createUserAccount($username, $email, $contact, $password)
+	{
+		$sql = "INSERT INTO users(username, email, contact, password)VALUES(:name, :email, :phone, :pass)";
+		$stmt = $this->conn->prepare($sql);
+		$stmt->execute(['uname' => $username, 'email' => $email, 'phone' => $contact, 'pass' => $password]);
+		return true;
+	}
+
+	/**
+	 * @param string $email A users's email
+	 * @return array
+	 * @desc Returns username and password records from db based on the method parameters
+	 */
+
+	public function loginIntoAccount($email)
+	{
+		$sql = "SELECT username, password, is_admin FROM users WHERE AND email = :email";
+		$stmt = $this->conn->prepare($sql);
+		$stmt->execute(['email' => $email]);
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+		return $result;
+	}
 }
