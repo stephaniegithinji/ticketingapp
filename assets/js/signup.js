9f320c358b7961555c60654e823d386fa9983db7
B4Ha-LzM17 <andruu.karanja>
2023-07-14 21:21:10 +0000

    Refactored sign-up form validation and error handling, and added success and error message display

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,141 +1,131 @@
-const signupForm = document.getElementById("signupForm");
-const signupFormTypeEl = document.querySelector('[name="formType-signup"]');
-const nameEl = document.querySelector('[name="username"]');
-const emailEl = document.querySelector('[name="email"]');
-const contactEl = document.querySelector('[name="contact"]');
-const passwordEl = document.querySelector('[name="password"]');
-const confirmPasswordEl = document.querySelector('[name="confirm_password"]');
-const msgSignup = document.querySelector('#signup small');
+// Select form elements
+const signupForm = document.querySelector("#signupForm");
+// Create an object with all elements needed from the form
+const formElements = {
+  formType: signupForm.querySelector('[name="formType-signup"]'),
+  username: signupForm.querySelector('[name="username"]'),
+  email: signupForm.querySelector('[name="email"]'),
+  contact: signupForm.querySelector('[name="contact"]'),
+  password: signupForm.querySelector('[name="password"]'),
+  confirmPassword: signupForm.querySelector('[name="confirm_password"]'),
+  message: document.querySelector('#signup small'),
+};
 
-const isRequiredOnSignUp = value => Boolean(value);
+// Set validation patterns for each input field
+const patterns = {
+  username: /^[a-zA-Z\s]{4,22}$/,
+  email: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  contact: /^(\+254|0)?[071]\d{8}$/,
+  password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/,
+  confirmPassword: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/
+};
 
 
+// Set error messages for each input field
+const messages = {
+  username: 'Username should only contain letters and be between 4 and 22 characters in length.',
+  email: 'Email is not valid.',
+  contact: 'Contact number must begin with +254 or 0 then followed by 1 or 7, and other 7 digits',
+  password: 'Password must have at least 8 characters that include at least 1 lowercase character, 1 uppercase character, 1 number, and 1 special character.',
+  confirmPassword: 'Passwords do not match.',
+};
 
-const isValidOnSignUp = (input, pattern, message) => {
+// Function to check if input value is required
+const isRequired = value => Boolean(value);
+
+// Function to validate input
+// Check if input value is required and matches the pattern
+// If not, it shows error
+const validateInput = (input, pattern, message) => {
   const value = input.value.trim();
-  if (!isRequiredOnSignUp(value)) {
-    showErrorOnSignUp(input, `${input.name} cannot be blank.`);
+  if (!isRequired(value)) {
+    showError(input, `${input.name} cannot be blank.`);
     return false;
   } else if (!pattern.test(value)) {
-    showErrorOnSignUp(input, message);
+    showError(input, message);
     return false;
   } else {
-    showSuccessOnSignUp(input);
+    showSuccess(input);
     return true;
   }
 };
 
-const showErrorOnSignUp = (element, message) => {
-  const inputField = element.parentElement;
-  const errorDisplay = inputField.querySelector('.error');
-
-  inputField.classList.add('error');
-  inputField.classList.remove('success');
-  errorDisplay.innerText = message;
+// Function to show validation errors
+// Changes the class of the form field and displays an error message
+const showError = (element, message) => {
+  const formField = element.parentElement;
+  formField.classList.remove('success');
+  formField.classList.add('error');
+  formField.querySelector('.error').innerText = message;
 };
 
-const showSuccessOnSignUp = element => {
-  const inputField = element.parentElement;
-  const errorDisplay = inputField.querySelector('.error');
-
-  errorDisplay.innerText = '';
-  inputField.classList.add('success');
-  inputField.classList.remove('error');
+// Function to show validation success
+// Changes the class of the form field and clears any error message
+const showSuccess = element => {
+  const formField = element.parentElement;
+  formField.classList.remove('error');
+  formField.classList.add('success');
+  formField.querySelector('.error').innerText = '';
 };
 
-// Function to display an error message in the sign-up form
-const displayErrorMessageOnSignup = (message) => {
-  msgSignup.innerHTML = `
-        <div class="alert error">
-            <p class="text">
-                <strong>${message}</strong>
-            </p>
-        </div>`;
-};
-
-// Function to display a success message in the sign-up form
-const displaySuccessMessageOnSignup = (message) => {
-  msgSignup.innerHTML = `
-        <div class="alert success">
-            <p class="text">
-                <strong>${message}</strong>
-            </p>
-        </div>`;
-};
-
-// Function to clear error/success messages in the sign-up form
-const clearMessagesOnSignup = () => {
-  msgSignup.innerHTML = '';
-};
-
-// Function to validate the sign-up form inputs
-const validateSignupForm = () => {
-  // Validate each input field and return true if all inputs are valid
-  const isNameValid = isValidOnSignUp(nameEl, /^[a-zA-Z\s]+$/, 'Username should only contain letters and be between 4 and 22 characters in length.');
-  const isEmailValid = isValidOnSignUp(emailEl, /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Email is not valid.');
-  const isContactValid = isValidOnSignUp(contactEl, /^(\+254|0)?[071]\d{8}$/, "Contact number must begin with +254 or 0 then followed by 1 or 7, and other 7 digits");
-  const isPasswordValid = isValidOnSignUp(passwordEl, /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/, 'Password must have at least 8 characters that include at least 1 lowercase character, 1 uppercase character, 1 number, and 1 special character.');
-  const checkPasswordMatch = () => {
-    const password = passwordEl.value.trim();
-    const confirmPassword = confirmPasswordEl.value.trim();
-    if (!isRequiredOnSignUp(confirmPassword)) {
-      showErrorOnSignUp(confirmPasswordEl, 'Confirm password cannot be blank.');
-      return false;
-    } else if (password !== confirmPassword) {
-      showErrorOnSignUp(confirmPasswordEl, 'Passwords do not match.');
-      return false;
-    } else {
-      showSuccessOnSignUp(confirmPasswordEl);
-      return true;
+// Function to validate form
+// Iterates over each form field and checks if it is valid
+// Checks if password and confirm password fields match
+const validateForm = () => {
+  let isValid = true;
+  for (let name in formElements) {
+    if (patterns[name]) {
+      isValid &= validateInput(formElements[name], patterns[name], messages[name]);
     }
-  };
-  const isConfirmPasswordValid = checkPasswordMatch();
-
-  return isNameValid && isEmailValid && isContactValid && isPasswordValid && isConfirmPasswordValid;
+  }
+  if (formElements.password.value !== formElements.confirmPassword.value) {
+    showError(formElements.confirmPassword, messages.confirmPassword);
+    isValid = false;
+  }
+  return isValid;
 };
 
-// Handle sign-up form submission
+// Function to display message
+// Displays a message on the form, success or error
+const displaySuccessMessageOnSignUp = (message, isError = false) => {
+  formElements.message.innerHTML = `
+    <div class="alert ${isError ? 'error' : 'success'}">
+      <p class="text">
+        <strong>${message}</strong>
+      </p>
+    </div>`;
+};
+
+// Handle form submission
+// On form submit, clears previous messages, validates form, and sends data to server
 signupForm.addEventListener('submit', async (e) => {
-  e.preventDefault(); // Prevent form submission
+  e.preventDefault();
+  formElements.message.innerHTML = ''; // Clear previous messages
 
-  clearMessagesOnSignup(); // Clear previous messages
-
-  if (validateSignupForm()) {
-    const formType = signupFormTypeEl.value;
-    const username = nameEl.value;
-    const email = emailEl.value;
-    const contact = contactEl.value;
-    const password = passwordEl.value;
-
+  if (validateForm()) {
     const requestBody = {
-      formType,
-      username,
-      email,
-      contact,
-      password
+      formType: formElements.formType.value,
+      username: formElements.username.value,
+      email: formElements.email.value,
+      contact: formElements.contact.value,
+      password: formElements.password.value,
     };
 
     try {
       const response = await fetch('assets/php/action.php', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(requestBody)
       });
 
+      const responseData = await response.json();
       if (response.ok) {
-        const responseData = await response.json();
-        if (responseData.success) {
-          displaySuccessMessageOnSignup(responseData.message);
-        } else {
-          displayErrorMessageOnSignup(responseData.message);
-        }
+        displaySuccessMessageOnSignUp(responseData.message, !responseData.success);
       } else {
         throw new Error('Network response was not OK.');
       }
     } catch (error) {
-      displayErrorMessageOnSignup('Some error occurred. Please try again.');
+      displaySuccessMessageOnSignUp('Some error occurred. Please try again.', true);
     }
   }
 });
