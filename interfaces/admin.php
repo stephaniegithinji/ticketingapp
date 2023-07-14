53f3904086fdebec15e1651329ce53fd2c05ee54
B4Ha-LzM17 <andruu.karanja>
2023-07-14 03:29:36 +0000

    Events cards loop, row organization, fetch data from db etc

--- interfaces/admin.php
+++ interfaces/admin.php
@@ -10,9 +10,17 @@ if (!isset($_SESSION['admin'])) {
 
 $admin = $_SESSION['admin'];
 
-// $interfaces = new Admin();
+$interfaces = new Admin();
 
-// $cards = $interfaces->fetchEvents();
+$cards_data = $interfaces->fetchEvents();
+
+// coalescing operator `??`
+// checks if a variable exists and is not null,
+// and if it doesn't, it returns a default value
+$message = $_SESSION['success'] ?? $_SESSION['error'] ?? null;
+// `unset()` function destroys a variable. Once a variable is unset, it's no longer accessible
+unset($_SESSION['success'], $_SESSION['error'], $_SESSION['contactUsMessage']);
+
 
 ?>
 
@@ -27,6 +35,7 @@ $admin = $_SESSION['admin'];
     <script src="https://kit.fontawesome.com/0d8179c479.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../assets/css/main.css">
     <link rel="stylesheet" href="../assets/css/modals.css">
+    <link rel="stylesheet" href="../assets/css/alerts.css">
     <link rel="stylesheet" href="../assets/css/cards.css">
     <link rel="stylesheet" href="../assets/css/admincards.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
@@ -38,7 +47,7 @@ $admin = $_SESSION['admin'];
         <div class="navbar">
             <h1 class="logo" style="font-size: 40px;">TickeTok </h1>
             <ul style="font-size: 15px;">
-                <li><a href="#">Hi <?= $admin ?></li>
+                <li><a href="#">Hi, <?= $admin ?></li>
 
                 <li><a href="#" id="addBtn">Create Event</a></li>
                 <li><a href="../assets/php/admin/logout.php">Logout</a></li>
@@ -47,60 +56,88 @@ $admin = $_SESSION['admin'];
         </div>
     </div>
     <br><br>
-    <div>
-        <p style="color: #fcc201; font-family: 'Fredericka the Great', cursive; font-size: 40px; margin-left: 130px">Upcoming Events</p>
-    </div>
-    <br><br>
+    <?= $message ?>
 
-
-    <!-- this row is the container with the card-->
-    <!-- <div class="card-container" style="margin-left: 130px; margin-bottom: 100px;">
-        <div class="column-item">
-            <div class="card" style="width: 33rem;">
-                <img class="card-img-top" src="../assets/images/p1.jpg" alt="Event Image">
-                <div class="card-body">
-                    <h3 class="card-title">Event Name</h3>
-                    <ul class="card-text">
-                        <li>Date:</li>
-                        <li>Venue:</li>
-                        <li>Time:</li>
-                        <li>Ticket Price:</li>
-                        <li>Tickets Available:</li>
-                    </ul>
-                    <br><br>
-                    <button id="editBtn" class="btn btn-primary">Edit </button>
-                    <br><br>
-                    <button id="deleteBtn" class="btn btn-danger">Delete</button>
-                    <br><br>
-                    <button class="btn btn-success">View Report</button>
-                </div>
-            </div>
+    <?php if (!$cards_data) : ?>
+        <div>
+            <p class="box">No Events at the moments!</p>
         </div>
-    </div> -->
+    <?php else : ?>
+        <?= str_repeat('<br>', 4); ?>
+        <p class="box">Events Line up</p>
+        <!-- this row is the container with the card-->
+        <div class="card-container" style="margin-left: 130px; margin-bottom: 100px">
+            <?php $rowCount = 0; ?>
+            <table class="cards-table">
+                <?php foreach ($cards_data as $card) : ?>
+                    <?php if ($rowCount % 3 === 0) : ?>
+                        <tr>
+                        <?php endif; ?>
+                        <td data-id="<?= $card['id'] ?>">
+                            <div class="column-item">
+                                <div class="card" style="width: 30rem;">
+                                    <input type="hidden" name="eventId">
+                                    <img class="card-img-top" src="<?= $card['banner'] ?>" alt="Event Image">
+                                    <div class="card-body">
+                                        <h2 class="card-title">Event Name: <?= $card['event_name'] ?></h2>
+                                        <ul class="card-text">
+                                            <li>Date: <?= date('M d, Y', strtotime($card['date'])) ?></li>
+                                            <li>Venue: <?= $card['venue'] ?></li>
+                                            <li>Time: <?= $card['time'] ?></li>
+                                            <li>Ticket Price: Ksh <?= number_format($card['ticket_price']) ?> each</li>
+                                            <li>Tickets Capacity: <?= $card['tickets_capacity'] ?> tickets</li>
+                                            <!-- <li>Tickets Available: <?= $card['tickets_capacity'] ?> tickets</li> -->
+                                        </ul>
+                                        <br><br>
+                                        <button id="editBtn" class="btn btn-primary">Edit </button>
+                                        <br><br>
+                                        <button id="deleteBtn" class="btn btn-danger">Delete</button>
+                                        <br><br>
+                                        <button class="btn btn-success">View Report</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
 
+                        <?php $rowCount++; ?>
 
+                        <?php if ($rowCount % 3 === 0 || $rowCount === count($cards_data)) : ?>
+                        </tr>
+                    <?php endif; ?>
+
+                <?php endforeach; ?>
+            </table>
+
+        </div>
+    <?php endif; ?>
 
     <!--Modal for Adding Events-->
     <div class="modal" id="addevent">
-        <div class="form-box" style="height: 800px;">
+        <div class="form-box" style="height: 820px;">
             <h1>Add Event</h1>
             <form method="POST" action="../assets/php/admin-action.php" enctype="multipart/form-data">
                 <div class="form-field">
+                    <label>Event Name:</label>
                     <input type="text" name="event_name" placeholder="Event Name">
                 </div>
                 <div class="form-field">
+                    <label>Event Date:</label>
                     <input type="date" name="date" placeholder="Event Date" min="<?php echo date('Y-m-d'); ?>" max="<?php echo date('Y-m-d', strtotime('+1 year')); ?>">
                 </div>
                 <div class="form-field">
+                    <label>Event venue:</label>
                     <input type="text" name="venue" placeholder="Venue">
                 </div>
                 <div class="form-field">
+                    <label>Event time:</label>
                     <input type="time" name="time" placeholder="Time">
                 </div>
                 <div class="form-field">
+                    <label>Ticket price:</label>
                     <input type="number" name="ticket_price" placeholder="Ticket Price">
                 </div>
                 <div class="form-field">
+                    <label>Event Name:</label>
                     <input type="number" name="tickets_available" placeholder="Tickets Available">
                 </div>
                 <div class="form-field">
@@ -117,7 +154,61 @@ $admin = $_SESSION['admin'];
 
 
 
+    <div class="modal" id="editEvent">
+        <div class="form-box" style="height: 770px">
+            <h2 id="event-name-edt"></h2>
+            <form method="POST" action="../assets/php/admin-action.php">
+                <input type="hidden" name="eventId" value="<?= $card['id'] ?>">
+                <div class="form-field">
+                    <label>Event Name:</label>
+                    <input type="text" name="event_name" placeholder="Event Name" value="<?= $card['event_name'] ?>">
+                </div>
+                <!-- <div class="form-field">
+                    <label>Event Date:</label>
+                    <input type="date" name="date"  value="<?= $card['date'] ?>">
+                </div> -->
+                <div class="form-field">
+                    <label>Event Venue:</label>
+                    <input type="text" name="venue" placeholder="Venue" value="<?= $card['venue'] ?>">
+                </div>
+                <div class="form-field">
+                    <label>Event Time:</label>
+                    <input type="time" name="time" placeholder="Time" value="<?= $card['time'] ?>">
+                </div>
+                <div class="form-field">
+                    <label>Tickets price:</label>
+                    <input type="number" name="ticket_price" placeholder="Ticket Price" value="<?= $card['ticket_price'] ?>">
+                </div>
+                <div class="form-field">
+                    <label>Tickets capacity:</label>
+                    <input type="number" name="ticket_capacity" placeholder="Tickets Available" value="<?= $card['tickets_capacity'] ?>">
+                </div>
+                <div class="btn-field">
+                    <button type="button" class="close" id="editClose">Close</button>
+                    <button type="submit" class="btn editEvent-btn" name="editEventBtn">Update &rarr;</button>
+                </div>
+            </form>
+            <br>
+        </div>
+    </div>
+
+    <div class="modal" id="deleteEvent">
+        <div class="form-box" style="height: 350px;">
+            <h1 id="event-name-edt-del"></h1>
+            <form method="POST" action="../assets/php/admin-action.php">
+                <input type="hidden" name="eventId" value="<?= $card['id'] ?>">
+                <h2>Are you sure you want to delete this Event?</h2>
+                <br>
+                <div class="btn-field">
+                    <button type="button" class="close" id="deleteClose">Close</button>
+                    <button type="submit" class="btn" name="deleteEventBtn">Delete &rarr;</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
     <!--Footer-->
+    <?= str_repeat('<br>', 4); ?>
     <footer class="footer">
         <div class="container">
             <div class="row">
--- interfaces/events.php
+++ interfaces/events.php
@@ -2,6 +2,7 @@
 
 require_once '../utils.php';
 
+require_once  '../assets/php/client_functions.php';
 
 if (!isset($_SESSION['client'])) {
     Utils::redirect_to('../index.php');
@@ -9,7 +10,19 @@ if (!isset($_SESSION['client'])) {
 
 $currentlyLoggedInUser = $_SESSION['client'];
 
+$interfaces = new Client();
+
+$cards_data = $interfaces->fetchEvents();
+
+// coalescing operator `??`
+// checks if a variable exists and is not null,
+// and if it doesn't, it returns a default value
+$message = $_SESSION['success'] ?? $_SESSION['error'] ?? null;
+// `unset()` function destroys a variable. Once a variable is unset, it's no longer accessible
+unset($_SESSION['success'], $_SESSION['error'], $_SESSION['contactUsMessage']);
+
 ?>
+
 
 <!DOCTYPE Html>
 <html lang="en">
@@ -21,125 +34,156 @@ $currentlyLoggedInUser = $_SESSION['client'];
     <link href="https://fonts.googleapis.com/css?family=Bad+Script|Comfortaa|Amiri|Cormorant+Garamond|Rancho|Fredericka+the+Great|Handlee|Homemade+Apple|Philosopher|Playfair+Display+SC|Reenie+Beanie|Unna|Zilla+Slab" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/main.css">
     <link rel="stylesheet" href="../assets/css/modals.css">
+    <link rel="stylesheet" href="../assets/css/alerts.css">
     <link rel="stylesheet" href="../assets/css/cards.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
 </head>
 
 <body>
 
+    <div class="navbar">
+        <h1 class="logo" style="font-size: 40px;">TickeTok </h1>
+        <ul style="font-size: 15px;">
+            <li><a href="#">Hi, <?= $currentlyLoggedInUser ?></li>
+            <li><a href="../assets/interfaces/history.php">My History</a></li>
+            <li><a href="../assets/php/logout.php">Logout</a></li>
+            <li><input type="text" id="search" name="search" placeholder="SEARCH"></li>
+        </ul>
+    </div>
+
+
+    <br><br>
+    <?= $message ?>
+
+    <?php if (!$cards_data) : ?>
+        <p class="box">Upcoming Events</p>
+    <?php endif; ?>
     <div>
-        <div class="navbar">
-            <h1 class="logo" style="font-size: 40px;">TickeTok </h1>
-            <ul style="font-size: 15px;">
-                <li><a href="#">Hi <?= $currentlyLoggedInUser ?></li>
-                <li><a href="../assets/interfaces/history.php">My History</a></li>
-                <li><a href="../assets/php/logout.php">Logout</a></li>
-                <li><input type="text" id="search" name="search" placeholder="SEARCH"></li>
-            </ul>
+
+
+        <!-- this row is the container with the card-->
+
+        <div class="card-container" style="margin-left: 50px; margin-bottom: 70px;">
+            <?php $rowCount = 0; ?>
+            <table class="cards-table">
+                <?php foreach ($cards_data as $card) : ?>
+                    <?php if ($rowCount % 3 === 0) : ?>
+                        <tr>
+                        <?php endif; ?>
+                        <td data-id="<?= $card['id'] ?>">
+                            <div class="column-item">
+                                <div class="card" style="width: 30rem">
+                                    <img class="card-img-top" src="<?= $card['banner'] ?>" alt="Event Image">
+                                    <div class="card-body">
+                                        <h3 class="card-title">Event Name: <?= $card['event_name'] ?></h3>
+                                        <ul class="card-text">
+                                            <li>Date: <?= date('M d, Y', strtotime($card['date'])) ?></li>
+                                            <li>Venue: <?= $card['venue'] ?></li>
+                                            <li>Time: <?= $card['time'] ?></li>
+                                            <li>Ticket Price: Ksh <?= number_format($card['ticket_price']) ?> each</li>
+                                        </ul>
+                                        <br><br>
+                                        <button type="button" id="buyingBtn" class="btn btn-primary">Buy Ticket &rarr;</button>
+                                        <br><br>
+                                        <button type="submit" id="qrCodeBtn" class="btn btn-primary">Generate QR Code</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+
+                        <?php $rowCount++; ?>
+
+                        <?php if ($rowCount % 3 === 0 || $rowCount === count($cards_data)) : ?>
+                        </tr>
+                    <?php endif; ?>
+
+                <?php endforeach; ?>
+            </table>
         </div>
-    </div>
 
-    <br><br>
-    <div>
-        <p style="color: #fcc201; font-family: 'Fredericka the Great', cursive; font-size: 40px; margin-left: 130px">Upcoming Events</p>
-    </div>
-    <br><br>
-
-    <!-- this row is the container with the card-->
-
-    <div class="card-container" style="margin-left: 130px; margin-bottom: 100px;">
-        <div class="column-item">
-            <!-- Loop this card-->
-            <div class="card" style="width: 33rem;">
-                <img class="card-img-top" src="../assets/images/p1.jpg" alt="Event Image">
-                <div class="card-body">
-                    <h5 class="card-title">Event Name</h5>
-                    <ul class="card-text">
-                        <li>Date:</li>
-                        <li>Venue:</li>
-                        <li>Time:</li>
-                        <li>Ticket Price:</li>
-                        <li>Tickets Available:</li>
-                    </ul>
-                    <br><br>
-                    <button type="button" id="buyingBtn" class="btn btn-primary">Buy Ticket &rarr;</button>
-                    <br><br>
-                    <button type="submit" id="qrCodeBtn" class="btn btn-primary">Generate QR Code</button>
-                </div>
+        <!--Modal for Buying Tickets-->
+        <div class="modal" id="buying">
+            <div class="form-box">
+                <h2 id="event-name-buy"></h2>
+                <form id="purchaseTicketForm" method="POST" action="../assets/php/action.php">
+                    <input type="hidden" name="eventId" value="<?= $card['id'] ?>">
+                    <div class="form-field">
+                        <select class="input-field" style="width:100%; height:40px" name="number_of_tickets">
+                            <option disabled>Number Of Tickets (Maximum - 5)</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                    <input type="hidden" name="ticket_price" value="<?= $card['ticket_price'] ?>">
+                    <div class="form-field">
+                        <select class="input-field" style="width:100%; height:40px;" name="mpesa">
+                            <option disabled selected>Payment Method</option>
+                            <option value="MPESA">M-PESA</option>
+                        </select>
+                    </div>
+                    <div class="btn-field">
+                        <button type="button" class="close" id="buyingClose">Close</button>
+                        <button type="submit" class="btn" name="purchase-ticket-btn">Pay &rarr;</button>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
 
-    <!--Modal for Buying Tickets-->
-    <div class="modal" id="buying">
-        <div class="form-box">
-            <h1>Ticket Purchase</h1>
-            <form method="POST" action="" onsubmit="">
-                <select class="input-field" style="width:100%; height:40px;" name="number_of_tickets">
-                    <option disabled selected>Number Of Tickets (Maximum - 5)</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-                <div class="error"></div>
-
-                <select class="input-field" style="width:100%; height:40px;" name="mpesa">
-                    <option disabled selected>Payment Method</option>
-                    <option value="MPESA">M-PESA</option>
-
-                </select>
+        <!--Modal for QR Code-->
+        <div class="modal" id="qr">
+            <div class="form-box">
+                <h1>Your e-Ticket</h1>
+                <h3 style="margin-top: 15px; color:red; margin-left: 10px; font-size:13px; text-align: center;">Do not close before scanning! Ticket will not be regenerated</h3>
+                <!--Should be displayed here-->
                 <div class="error"></div>
                 <div class="btn-field">
-                    <button type="button" style="background-color: #999999; margin-top: 180px;" id="buyingClose">Close</button>
-                    <button type="submit" style="background-color: green; margin-top: 180px;" name="pay">Pay &rarr;</button>
+                    <button type="button" class="btn btn-primary" style="margin-top: 260px; margin-left: 100px;" id="qrClose"> Ok &rarr;</button>
                 </div>
-            </form>
-        </div>
-    </div>
-
-    <!--Modal for QR Code-->
-    <div class="modal" id="qr">
-        <div class="form-box">
-            <h1>Your e-Ticket</h1>
-            <h3 style="margin-top: 15px; color:red; margin-left: 10px; font-size:13px; text-align: center;">Do not close before scanning! Ticket will not be regenerated</h3>
-            <!--Should be displayed here-->
-            <div class="error"></div>
-            <div class="btn-field">
-                <button type="button" class="btn btn-primary" style="margin-top: 260px; margin-left: 100px;" id="qrClose"> Ok &rarr;</button>
-            </div>
-            </form>
-        </div>
-    </div>
-
-
-    <!--Footer-->
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="footer-col">
-                    <h4>company</h4>
-                    <ul>
-                        <li><a href="../assets/index.php">About Us</a></li>
-                        <li><a href="../assets/index.php">Our Services</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>get help</h4>
-                    <ul>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Refunds</a></li>
-                        <li><a href="#">Terms of Service</a></li>
-                        <li><a href="#">Payment Options</a></li>
-                    </ul>
-                </div>
-
+                </form>
             </div>
         </div>
-    </footer>
-    <script src='../assets/js/clientmodals.js'></script>
+
+
+        <!--Footer-->
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col">
+                        <h4>company</h4>
+                        <ul>
+                            <li><a href="../assets/index.php">About Us</a></li>
+                            <li><a href="../assets/index.php">Our Services</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>get help</h4>
+                        <ul>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Refunds</a></li>
+                            <li><a href="#">Terms of Service</a></li>
+                            <li><a href="#">Payment Options</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+        <script src='../assets/js/clientmodals.js'></script>
+        <script>
+            const form = document.getElementById("purchaseTicketForm");
+
+            const selectField = document.querySelector('[name="number_of_tickets"]');
+            form.addEventListener("submit", function(event) {
+                if (selectField.value === null || selectField.value === "") {
+                    event.preventDefault(); // Prevent form submission
+                    alert("Please select an option for number of tickets.");
+                }
+            });
+        </script>
 </body>
 
 </html>
