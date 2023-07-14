3e56924fe20ef9131a7dffda3101497c6ea79c68
B4Ha-LzM17 <andruu.karanja>
2023-07-14 21:21:19 +0000

    Refactor sign-in form validation and submission logic, and improve error and success message handling

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -1,123 +1,83 @@
-const signinForm = document.getElementById("signinForm");
-        const signinFormTypeEl = document.querySelector('[name="formType-signin"]');
-        const signinEmailEl = document.querySelector('[name="signin-email"]');
-        const signinPasswordEl = document.querySelector('[name="signin-password"]');
-        const msgSignin = document.querySelector('#signin small');
+// Get the elements
+const form = document.querySelector('#signinForm');
+const emailEl = form.elements['signin-email'];
+const passwordEl = form.elements['signin-password'];
+const msgEl = document.querySelector('#signin small');
 
-        const showErrorOnSignIn = (element, message) => {
-            const inputField = element.parentElement;
-            const errorDisplay = inputField.querySelector('.error');
+// The URLs for redirection based on the response message
+const URLs = {
+  admin: 'http://127.0.0.1/ticketingapp/interfaces/admin.php',
+  client: 'http://127.0.0.1/ticketingapp/interfaces/events.php',
+};
 
-            errorDisplay.innerText = message;
-            inputField.classList.add('error');
-            inputField.classList.remove('success');
-        };
+// Validate input and show error or success
+const validateAndShowStatus = (input, pattern, blankMsg, invalidMsg) => {
+  const value = input.value.trim();
+  const formField = input.parentElement;
+  const errorEl = formField.querySelector('.error');
 
-        const showSuccessOnSignIn = element => {
-            const inputField = element.parentElement;
-            const errorDisplay = inputField.querySelector('.error');
+  formField.classList.remove('error', 'success');
+  errorEl.innerText = '';
 
-            errorDisplay.innerText = '';
-            inputField.classList.add('success');
-            inputField.classList.remove('error');
-        };
+  if (!value) {
+    formField.classList.add('error');
+    errorEl.innerText = blankMsg;
+    return false;
+  } else if (!pattern.test(value)) {
+    formField.classList.add('error');
+    errorEl.innerText = invalidMsg;
+    return false;
+  } else {
+    formField.classList.add('success');
+    return true;
+  }
+};
 
-        // Function to display an error message in the sign-in form
-        const displayErrorMessageOnSignin = (message) => {
-            msgSignin.innerHTML = `
-        <div class="alert error">
-            <p class="text">
-                <strong>${message}</strong>
-            </p>
-        </div>`;
-        };
+// Display a message in the form
+const displayMessage = (msg, isError = false) => {
+  msgEl.innerHTML = `
+    <div class="alert ${isError ? 'error' : 'success'}">
+        <p class="text">
+            <strong>${msg}</strong>
+        </p>
+    </div>`;
+};
 
-        const displaySuccessMessageOnSignin = (message) => {
-            msgSignin.innerHTML = `
-        <div class="alert success">
-            <p class="text">
-                <strong>${message}</strong>
-            </p>
-        </div>`;
-        };
+// Handle form submission
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-        const isRequiredOnSignIn = value => Boolean(value);
+  msgEl.innerHTML = '';
 
-        const isValidOnSignIn = (input, pattern, message) => {
-            const value = input.value.trim();
-            if (!isRequiredOnSignIn(value)) {
-                showErrorOnSignIn(input, `${input.name} cannot be blank.`);
-                return false;
-            } else if (!pattern.test(value)) {
-                showErrorOnSignIn(input, message);
-                return false;
-            } else {
-                showSuccessOnSignIn(input);
-                return true;
-            }
-        };
+  const isEmailValid = validateAndShowStatus(emailEl, /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Email cannot be blank.', 'Email is not valid.');
+  const isPasswordValid = validateAndShowStatus(passwordEl, /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/, 'Password cannot be blank.', 'Password must have at least 8 characters that include at least 1 lowercase character, 1 uppercase character, 1 number, and 1 special character.');
 
-        // Function to clear error/success messages in the sign-in form
-        const clearMessagesOnSignin = () => {
-            msgSignin.innerHTML = '';
-        };
+  if (!isEmailValid || !isPasswordValid) return;
 
-        // Function to validate the sign-in form inputs
-        const validateSigninForm = () => {
-            // Validate each input field and return true if all inputs are valid
-            const isEmailValid = isValidOnSignIn(signinEmailEl, /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Email is not valid.');
-            const isPasswordValid = isValidOnSignIn(signinPasswordEl, /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/, 'Password must have at least 8 characters that include at least 1 lowercase character, 1 uppercase character, 1 number, and 1 special character.');
+  const data = {
+    formType: 'signin',
+    email: emailEl.value,
+    password: passwordEl.value,
+  };
 
-            return isEmailValid && isPasswordValid;
-        };
+  try {
+    const response = await fetch('assets/php/action.php', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
 
-        // Handle sign-in form submission
-        signinForm.addEventListener('submit', async (e) => {
-            e.preventDefault(); // Prevent form submission
-
-            clearMessagesOnSignin(); // Clear previous messages
-
-            if (validateSigninForm()) {
-                const formType = signinFormTypeEl.value;
-                const email = signinEmailEl.value;
-                const password = signinPasswordEl.value;
-
-                const requestBody = {
-                    formType,
-                    email,
-                    password
-                };
-
-                try {
-                    const response = await fetch('assets/php/action.php', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(requestBody)
-                    });
-
-                    if (response.ok) {
-                        const responseData = await response.json();
-                        if (responseData.success) {
-                            switch (responseData.message) {
-                                case 'admin':
-                                    window.location.href = 'http://127.0.0.1/ticketingapp/interfaces/admin.php';
-                                    break;
-                                case 'client':
-                                    window.location.href = 'http://127.0.0.1/ticketingapp/interfaces/events.php';
-                                    break;
-                                default:
-                                    displayErrorMessageOnSignin(`case ${responseData.message} not implemented yet`);
-                                    break;
-                            }
-                        } else {
-                            displayErrorMessageOnSignin(responseData.message);
-                        }
-
-                    }
-                } catch (error) {
-                    displayErrorMessageOnSignin('An error occurred during the network call.');
-                }
-            }
-        });
+    if (response.ok) {
+      const { success, message } = await response.json();
+      if (success) {
+        const URL = URLs[message];
+        if (URL) window.location.href = URL;
+        else displayMessage(`Case ${message} not implemented yet`, true);
+      } else {
+        displayMessage(message, true);
+      }
+    }
+  } catch {
+    displayMessage('An error occurred during the network call.', true);
+  }
+});
