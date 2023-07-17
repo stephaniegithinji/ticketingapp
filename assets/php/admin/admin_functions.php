0ea751f0d46eb65b37d77cb7d0e7b9244bee479b
B4Ha-LzM17 <andruu.karanja>
2023-07-17 13:16:15 +0000

    Add event, fetch events, edit event, and delete event functionality to the Admin class

--- assets/php/admin/admin_functions.php
+++ assets/php/admin/admin_functions.php
@@ -6,55 +6,102 @@ require_once __DIR__ .  '/../../../connect/config.php';
 class Admin extends Db
 {
 
-    public function addEvent($event_name, $date, $venue, $time, $ticket_price,  $tickets_capacity, $thumbnail)
+	/**
+	 * Add a new event to the events table.
+	 *
+	 * @param string $event_name The name of the event.
+	 * @param string $date The date of the event.
+	 * @param string $venue The venue of the event.
+	 * @param string $time The time of the event.
+	 * @param int $ticket_price The ticket price of the event.
+	 * @param int $tickets_capacity The capacity of tickets for the event.
+	 * @param string $thumbnail The banner image for the event.
+	 * @param string|null $from_date The starting date of a date range event (nullable).
+	 * @param string|null $to_date The ending date of a date range event (nullable).
+	 * @return bool True if the event is added successfully, false otherwise.
+	 */
+	public function addEvent($event_name, $date, $venue, $time, $ticket_price, $tickets_capacity, $thumbnail, $from_date, $to_date)
 	{
-		$sql = "INSERT INTO events(event_name, date, venue, time, ticket_price, tickets_capacity, banner, created_at, updated_at) VALUES(:event_name, :date, :venue, :time, :ticket_price, :tickets_capacity, :banner, NOW(), '0000-00-00 00:00:00')";
+		$sql = "INSERT INTO events (event_name, date, venue, time, ticket_price, tickets_capacity, banner, created_at, updated_at, from_date, to_date) 
+            VALUES (:event_name, :date, :venue, :time, :ticket_price, :tickets_capacity, :banner, NOW(), '0000-00-00 00:00:00', :from_date, :to_date)";
 		$stmt = $this->conn->prepare($sql);
-		$stmt->execute(['event_name' => $event_name, 'date' => $date, 'venue' => $venue, 'time' => $time, 'ticket_price' => $ticket_price, 'tickets_capacity' => $tickets_capacity, 'banner' => $thumbnail]);
+		$stmt->execute([
+			'event_name' => $event_name,
+			'date' => $date,
+			'venue' => $venue,
+			'time' => $time,
+			'ticket_price' => $ticket_price,
+			'tickets_capacity' => $tickets_capacity,
+			'banner' => $thumbnail,
+			'from_date' => $from_date,
+			'to_date' => $to_date
+		]);
 		return true;
 	}
 
-
-    public function fetchEvents()
+	/**
+	 * Fetch all events from the events table.
+	 *
+	 * @return array An array of events.
+	 */
+	public function fetchEvents()
 	{
-		$sql = "SELECT * FROM events ORDER BY date DESC";
+		$sql = "SELECT *,
+            CASE
+                WHEN date IS NULL THEN CONCAT(from_date, ' - ', to_date)
+                ELSE date
+            END AS event_date,
+            CASE
+                WHEN date IS NULL THEN from_date
+                ELSE NULL
+            END AS event_from_date,
+            CASE
+                WHEN date IS NULL THEN to_date
+                ELSE NULL
+            END AS event_to_date
+            FROM events
+            WHERE (date >= CURDATE() OR from_date >= CURDATE())
+            ORDER BY date ASC";
 		$stmt = $this->conn->prepare($sql);
 		$stmt->execute();
 		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		return $result;
 	}
 
-    // public function editEvent($id, $date, $venue, $time, $ticket_price)
-	// {
-		
-	// 	return true;
-	// }
 
-	public function editEvent($id, $evname,  $venue, $time, $ticket_price, $tickets_capacity)
 
+	/**
+	 * Edit an existing event in the events table.
+	 *
+	 * @param int $id The ID of the event to edit.
+	 * @param string $evname The updated name of the event.
+	 * @param string $venue The updated venue of the event.
+	 * @param string $date The updated date of the event.
+	 * @param string $time The updated time of the event.
+	 * @param int $ticket_price The updated ticket price of the event.
+	 * @param int $tickets_capacity The updated capacity of tickets for the event.
+	 * @return bool True if the event is edited successfully, false otherwise.
+	 */
+	public function editEvent($id, $evname, $venue, $date, $from_date, $to_date, $time, $ticket_price, $tickets_capacity)
 	{
-		$sql = "UPDATE events SET event_name = :name, venue = :loc, time = :time, ticket_price = :tck_price, tickets_capacity = :tickets_capacity, updated_at = NOW() WHERE id = :id";
+		$sql = "UPDATE events SET event_name = :name, date = :dt, from_date = :from_date, to_date = :to_date, venue = :loc, time = :time, ticket_price = :tck_price, tickets_capacity = :tickets_capacity, updated_at = NOW() WHERE id = :id";
 		$stmt = $this->conn->prepare($sql);
-		$stmt->execute(['id' => $id, 'name' => $evname, 'loc' => $venue, 'time' => $time, 'tck_price' => $ticket_price, 'tickets_capacity' => $tickets_capacity]);
+		$stmt->execute(['id' => $id, 'name' => $evname, 'dt' => $date, 'from_date' => $from_date, 'to_date' => $to_date, 'loc' => $venue, 'time' => $time, 'tck_price' => $ticket_price, 'tickets_capacity' => $tickets_capacity]);
 		return true;
 	}
 
 
+	/**
+	 * Delete an event from the events table.
+	 *
+	 * @param int $id The ID of the event to delete.
+	 * @return bool True if the event is deleted successfully, false otherwise.
+	 */
 	public function deleteEvent($id)
 	{
-    	$sql = "DELETE FROM events WHERE id = :id";
-    	$stmt = $this->conn->prepare($sql);
-    	$stmt->execute(['id' => $id]);
-     	return true;
-	}
-
-
-	/* Ask Baha what's going on here */
-    public function getIdSOfEvents()
-	{
-		$sql = "SELECT * FROM events";
+		$sql = "DELETE FROM events WHERE id = :id";
 		$stmt = $this->conn->prepare($sql);
-		$stmt->execute();
-		return $stmt->rowCount(); //37
+		$stmt->execute(['id' => $id]);
+		return true;
 	}
 }
